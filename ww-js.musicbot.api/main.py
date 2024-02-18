dba2532f19e292da4072f3231a7c41b85d225877
morrischoga <mchoga@outlook.com>
2024-02-18 19:13:42 +0100

    init

--- ww-js.musicbot.api/main.py
+++ ww-js.musicbot.api/main.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
+
+    print("Main called")
     requested_song = request.get_json()
     song_metadata = get_song_metadata(f"{requested_song['key']}")
 
     song = download(song_metadata["title"],song_metadata["video_id"],os.path.join("/usr/src/api","songs"))
     tagger(song_metadata["title"],song_metadata["artist"],song_metadata["album_name"],song_metadata["url"],song)
 
-
+    print(song)
     return song
 
 
