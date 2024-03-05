0cfaa0bbc1a63ead2129bbabb75f88f779d2bd2c
morrischoga <mchoga@outlook.com>
2024-03-05 17:49:16 +0100

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -232,8 +232,8 @@ client.on('message', async (message) => {
             Object.keys(registeredUsers).includes(userID) ? await (async function () {
 
                 if (registeredUsers[userID][1] < 10) {
-                    // await sendSong(message,registeredUsers,userID)
-                    await message.reply("The bot is undergoing maintenance. Contact the admin to offer support for the project 😊")
+                    await sendSong(message,registeredUsers,userID)
+                    // await message.reply("The bot is undergoing maintenance. Contact the admin to offer support for the project 😊")
 
                 } else {
                     message.reply("You have exceeded your daily limit...")
@@ -244,8 +244,8 @@ client.on('message', async (message) => {
                 userInfo.records[0].fields.userName = userName
                 userInfo.records[0].fields.userCountry = userCountry
                 await addUser()
-                await message.reply("The bot is undergoing maintenance. Contact the admin to offer support for the project 😊")
-                // await sendSong(message,registeredUsers,userID)
+                // await message.reply("The bot is undergoing maintenance. Contact the admin to offer support for the project 😊")
+                await sendSong(message,registeredUsers,userID)
             })()
 
 
