69c322a4d5a896e9148fae595ee8bca8163ab6b2
morrischoga <mchoga@outlook.com>
2024-03-15 19:37:27 +0100

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -227,7 +227,7 @@ client.on('message', async (message) => {
     if (message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && isGroup) {
 
 
-        if ((await message.getChat()).id.user === "120363213455576189" || (await message.getChat()).id.user === "2348034690865-1596391813" || (await message.getChat()).id.user === "120363223962652835") {
+        if ((await message.getChat()).id.user === "120363243170575745" || (await message.getChat()).id.user === "2348034690865-1596391813" || (await message.getChat()).id.user === "120363223962652835") {
 
             let userID = (await message.id.participant).substring(0,(await message.id.participant).indexOf('@'))
             let userName = await message._data.notifyName
