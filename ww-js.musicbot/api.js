15e33bc4af1e71b8aa038d697b960299d328a382
morrischoga <mchoga@outlook.com>
2024-03-24 14:24:01 +0100

    init

--- ww-js.musicbot/api.js
+++ ww-js.musicbot/api.js
@@ -33,7 +33,13 @@ const addUser = async (userInfo) => {
         method: 'POST',
         airtableHeaders,
         body: JSON.stringify(userInfo)
-    }).then((a)=>console.log(a.body)).catch(error=>{
+    }).then((a)=>{
+        console.log(a.body)
+        console.log(typeof a.body)
+        console.log(a)
+
+
+    }).catch(error=>{
         console.log(`An error occurred while addingUser https://api.airtable.com: ${error}`)
     })
 
