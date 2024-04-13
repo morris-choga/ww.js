e3b4238dd40957ed3e0e1617eb4ca92581d83803
morrischoga <mchoga@outlook.com>
2024-04-13 12:52:19 +0200

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -25,7 +25,7 @@ class Bot{
                 clientId: `${sessionName}`
             }),
             puppeteer: {
-                headless: true,
+                headless: false,
                 args: [
                     '--no-sandbox',
                     // '--disable-setuid-sandbox',
@@ -296,20 +296,20 @@ class Bot{
 
 }
 
-// const bot1 = new Bot("0683",[0,1]);
+const bot1 = new Bot("0683",[0,1]);
 const bot2 = new Bot("3202",[2,3]);
-// const bot3 = new Bot("9554",[4,5,6]);
+const bot3 = new Bot("9554",[4,5,6]);
 const bot4 = new Bot("4221",[7,8,9]);
 //
 // const bot5 = new Bot("4789",[0,1,2,3,4]);
 // const bot6 = new Bot("9552",[5,6,7,8,9]);
 
-// bot1.initialize();
-// bot2.initialize();
-// bot3.initialize();
+bot1.initialize();
+bot2.initialize();
+bot3.initialize();
 bot4.initialize();
 
-// bot5.initialize();
+// bot5.initialize(); banned
 // bot6.initialize();
 
 
