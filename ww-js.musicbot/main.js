9339ae985886b7322a733549cdda106af68379b9
morrischoga <mchoga@outlook.com>
2024-04-18 13:27:54 +0200

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -26,7 +26,7 @@ class Bot{
                 clientId: `${sessionName}`
             }),
             puppeteer: {
-                headless: false,
+                headless: true,
                 args: [
                     '--no-sandbox',
                     // '--disable-setuid-sandbox',
