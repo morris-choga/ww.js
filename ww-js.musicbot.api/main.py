e395f5d80f798a877b79bcf1d6c5943ad7d5560e
morrischoga <mchoga@outlook.com>
2024-05-10 17:52:13 +0200

    init

--- ww-js.musicbot.api/main.py
+++ ww-js.musicbot.api/main.py
@@ -73,4 +73,4 @@ def get_status():
 if __name__ =="__main__":
     # app.run(debug=True,use_reloader=False)
     # app.run(debug=True,use_reloader=False,host='0.0.0.0')
-    app.run(debug=True,use_reloader=True,host='0.0.0.0')
+    app.run(debug=True,use_reloader=True,host='0.0.0.0',port=1080)
