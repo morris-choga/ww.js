eb36c6b55f73e15344f9281a9760a4a06732e9f6
morrischoga <mchoga@outlook.com>
2024-05-19 08:46:40 +0200

    init

--- ww-js.musicbot.api/songmetadata.py
+++ ww-js.musicbot.api/songmetadata.py
@@ -68,7 +68,8 @@ def get_albums_metadata(album):
     if len(results) > 0:
         count = 0
         for x in results:
-            if count < 2:
+            if count < 3:
+                
                 albums.append({"artist":x['artists'][1]['name'], "title":x['title'],"album_id":x['browseId'],"year":x['year'],"thumbnail":x['thumbnails'][-1]['url']})
                 count += 1
 
