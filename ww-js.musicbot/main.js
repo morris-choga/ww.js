f0a4620fcd464b131be74fb0081fb9e7c9f111a5
morrischoga <mchoga@outlook.com>
2024-06-02 11:19:33 +0200

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -33,8 +33,8 @@ class Bot{
             }),
             puppeteer: {
                 headless: true,
-                executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-
+                // executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+                executablePath: "/usr/src/app/chrome/google-chrome-stable",
 
                 args: [
                     '--no-sandbox',
