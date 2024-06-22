d5cfccdc99027314ba28f78e2c47b313abbba6c3
morrischoga <mchoga@outlook.com>
2024-06-22 15:37:29 +0200

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -546,8 +546,8 @@ class Bot{
 
 
 
-const bot1 = new Bot("8573",[0,1,2,3,4]);
-const bot2 = new Bot("6159",[5,6,7,8,9]);
+const bot1 = new Bot("8573",[0,1,2,8,4]);
+const bot2 = new Bot("6159",[5,6,7,3,9]);
 // const bot3 = new Bot("8573",[3,6,7,8,9]);
 
 
