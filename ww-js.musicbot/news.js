406baa7fa7b0621b9bd0d97c48a38b813577cab9
morrischoga <mchoga@outlook.com>
2024-07-10 00:53:59 +0200

    init

--- ww-js.musicbot/news.js
+++ ww-js.musicbot/news.js
@@ -1,10 +1,15 @@
 const {fetchHiphopDX, lastNewsUpdate, updateLastNewsTime} = require("./api");
 const {sendNews} = require("./messenger");
+const log = require("qrcode-terminal");
 
 
 const hiphopDXNews = async (client) =>{
 
-    let news = await fetchHiphopDX()
+    let news = await fetchHiphopDX().catch((error)=>{
+
+        console.log(`Error: ${error}`)
+        return "2024-07-09T08:00:07-07:00"
+    })
 
     const promises = Object.keys(news).map(async (key) => {
 
