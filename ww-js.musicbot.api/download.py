b818fa564d89029f0e5c7d23c55fe594b17c74f7
morrischoga <mchoga@outlook.com>
2025-02-18 16:04:24 +0100

    init

--- ww-js.musicbot.api/download.py
+++ ww-js.musicbot.api/download.py
@@ -10,7 +10,7 @@ from songmetadata import tagger, get_playlist, get_songs_metadata
 from downloaded_albums import album_ids
 # from moviepy.editor import *
 os.environ["FFMPEG_BINARY"] = "/usr/bin/ffmpeg"
-from moviepy.editor import AudioFileClip
+# from moviepy.editor import AudioFileClip
 
 oauth = f"{os.getcwd()}/oauth.json"
 from pytubefix import Playlist
@@ -65,10 +65,10 @@ def download_song(video_id, location):
 
     try:
 
-        # mp4_no_frame = vid_file
+        mp4_no_frame = vid_file
 
 
-        mp4_no_frame = AudioFileClip(vid_file)
+        # mp4_no_frame = AudioFileClip(vid_file)
 
         mp4_no_frame.write_audiofile(audio_file, logger=None)
         mp4_no_frame.close()
