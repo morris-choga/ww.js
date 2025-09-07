4dbbc2cfd1e9237c3793324c78f00aea70314266
morrischoga <mchoga@outlook.com>
2025-09-07 20:09:01 +0200

    init

--- ww-js.musicbot/main.js
+++ ww-js.musicbot/main.js
@@ -27,7 +27,7 @@ class Bot{
         this.client = new Client({
             // ...,
             pairWithPhoneNumber: {
-                phoneNumber: '48796224294',
+                phoneNumber: '263782493985',
                 showNotification: true
             },
 
